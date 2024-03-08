d2518cfc2099be8ff07d1c4fd97beef1229733d1
= <taliraj@outlook.com>
2024-03-08 22:56:27 +0000

    feat: add set.lua and delete kickstart folder

---
+++ lua/custom/plugins/set.lua
@@ -0,0 +1,14 @@
+-- vim.opt.guicursor = ''
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.wrap = false
+
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+return {}
