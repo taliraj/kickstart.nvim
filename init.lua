f4e20b538fa136b04e755a30abceda7a962f9a35
= <taliraj@outlook.com>
2024-03-11 22:52:50 +0000

    feat: add mini starter

--- init.lua
+++ init.lua
@@ -813,6 +813,8 @@ require('lazy').setup({
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
+      require('mini.starter').setup()
+
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
