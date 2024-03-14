845bcddceaa88c7c58d93c71c1b30d4fdb5f18c4
= <taliraj@outlook.com>
2024-03-14 17:51:03 +0000

    feat: add folding and new themes

--- lua/custom/plugins/set.lua
+++ lua/custom/plugins/set.lua
@@ -1,5 +1,3 @@
--- vim.opt.guicursor = ''
-
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -7,8 +5,15 @@ vim.opt.expandtab = true
 
 vim.opt.wrap = false
 
--- vim.opt.incsearch = true
-
 vim.opt.termguicolors = true
+
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldenable = false
+vim.opt.foldcolumn = '0'
+vim.opt.foldtext = ''
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 1
+vim.opt.foldnestmax = 4
 
 return {}
