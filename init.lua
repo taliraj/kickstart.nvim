9e94968604d6dcf763362e03859ebf8ddd3450d9
tau <taliraj@outlook.com>
2024-04-29 08:46:44 +0100

    feat: activate which key

--- init.lua
+++ init.lua
@@ -178,14 +178,12 @@ require('lazy').setup({
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        -- ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-        -- ['<leader>t'] = { name = '[T]elescope Workspace', _ = 'which_key_ignore' },
+        ['<leader>t'] = { name = '[T]elescope Workspace', _ = 'which_key_ignore' },
         ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
       }
       -- visual mode
       require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
+        ['<leader>H'] = { 'Git [H]unk' },
       }, { mode = 'v' })
     end,
   },
