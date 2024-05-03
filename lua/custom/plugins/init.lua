48979f05709c036114921a5127f7605d7b7d9b37
tau <taliraj@outlook.com>
2024-05-03 18:13:32 +0100

    chore: add colorchemes

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -19,4 +19,6 @@ return {
   { 'rebelot/kanagawa.nvim' },
   { 'rose-pine/neovim' },
   { 'github/copilot.vim' },
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+  { 'echasnovski/mini.nvim', version = false },
 }
