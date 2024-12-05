06c7d61bf9e5785f816870099d0761da91e2063c
taliraj <taliraj@outlook.com>
2024-12-05 18:55:37 +0000

    feat: change colorschem

--- init.lua
+++ init.lua
@@ -602,8 +602,8 @@ require('lazy').setup({
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      vim.cmd.colorscheme 'catppuccin'
-      -- vim.cmd.colorscheme 'rose-pine'
+      -- vim.cmd.colorscheme 'catppuccin'
+      vim.cmd.colorscheme 'rose-pine'
       -- vim.cmd.colorscheme 'minischeme'
       -- You can configure highlights by doing something like:
       -- vim.cmd.hi 'Comment gui=none'
