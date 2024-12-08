7bc24a90ff4c1cdfdf5135f28d9f7c2dfa1daac3
taliraj <taliraj@outlook.com>
2024-12-08 01:03:21 +0000

    chore: remove comments

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -9,10 +9,7 @@
 
 vim.keymap.set('n', '<leader>e', vim.cmd.Ex, { desc = 'Explorer' })
 vim.keymap.set('i', 'jj', '<Esc>')
--- vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>', { desc = '[L]azy[G]it' })
 vim.keymap.set('n', '<leader>c', ':bdelete<CR>', { desc = 'Close Buffer' })
 vim.keymap.set('n', '<leader>w', ':write<CR>', { desc = 'Save' })
 
-return {
-  { 'echasnovski/mini.nvim', version = false },
-}
+return {}
